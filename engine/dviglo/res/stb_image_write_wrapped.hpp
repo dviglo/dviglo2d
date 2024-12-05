599c276d3112bed436c38d98d29c104d96582e73
1vanK <1vanK@users.noreply.github.com>
2024-12-06 01:56:28 +0300

    stb_image_write_wrapped.hpp: Отступы

--- engine/dviglo/res/stb_image_write_wrapped.hpp
+++ engine/dviglo/res/stb_image_write_wrapped.hpp
@@ -5,16 +5,16 @@
 #pragma once
 
 #ifdef INCLUDE_STB_IMAGE_WRITE_H
-#error "Don't include <stb_image_write.h>"
+    #error "Don't include <stb_image_write.h>"
 #endif
 
 #if defined(__GNUC__) // GCC, Clang или MinGW
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
 #include <stb_image_write.h>
 
 #if defined(__GNUC__) // GCC, Clang или MinGW
-#pragma GCC diagnostic pop
+    #pragma GCC diagnostic pop
 #endif
